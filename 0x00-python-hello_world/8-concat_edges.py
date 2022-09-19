3717b13cb02d3249f6bf63d920cf27042e233d4f
mug3njutsu <johngitahi272@gmail.com>
2022-09-20 01:02:31 +0300

    Task 9

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str_list = str.split(" ")
-print(str_list[5:6][0], str_list[6], str_list[-4], str_list[0])
+print(str.split(" ")[5:6][0], str.split(" ")[6], str.split(" ")[-4], str.split(" ")[0])
