4382e39cdf18d89ef63b704a5d883706c5dd1333
mug3njutsu <johngitahi272@gmail.com>
2022-09-20 00:52:29 +0300

    Task 7

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = {"first":str1, "second":str2}
+str3 = {'first': str1, 'second': str2}
 print("Welcome to {w[first]} {w[second]}!".format(w=str3))
