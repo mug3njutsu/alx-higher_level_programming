b970515505d38e417c74d317d70082b6e123e43b
mug3njutsu <johngitahi272@gmail.com>
2022-09-20 14:10:07 +0300

    Task 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,14 +5,19 @@ last_digit = int(str(number)[-1])
 if number < 0:
     if last_digit == 0:
         last_digit = last_digit
-        print(f"Last digit of {number:d} is {last_digit:d} and is {last_digit:d}")
+        print(f"Last digit of {number:d} is {last_digit:d} \
+and is {last_digit:d}")
     else:
         last_digit = -abs(last_digit)
-        print(f"Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")
+        print(f"Last digit of {number:d} is {last_digit:d} \
+and is less than 6 and not 0")
 else:
     if last_digit == 0:
-        print(f"Last digit of {number:d} is {last_digit:d} and is {last_digit:d}")
+        print(f"Last digit of {number:d} is {last_digit:d} \
+and is {last_digit:d}")
     elif last_digit <= 5:
-        print(f"Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")
+        print(f"Last digit of {number:d} is {last_digit:d} \
+and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
+        print(f"Last digit of {number:d} is {last_digit:d} \
+and is greater than 5")
