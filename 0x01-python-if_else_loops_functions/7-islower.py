3ec78d91f088f64f1a1dde2f1307fdead016edad
mug3njutsu <johngitahi272@gmail.com>
2022-09-20 15:30:19 +0300

    Task 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if char in list(map(chr, range(ord('a'), ord('z')+1))):
+    if c in list(map(chr, range(ord('a'), ord('z')+1))):
         return True
     else:
         return False
