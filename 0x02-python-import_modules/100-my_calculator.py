006892df2f80e39925b6b517e56c0dc54f1ace66
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 19:10:31 +0300

    Task 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,6 +7,9 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    elif sys.argv[2] not in "+-*/":
+    	print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     else:
         a = int(argv[1])
         b = int(argv[3])
@@ -18,6 +21,3 @@ if __name__ == "__main__":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif argv[2] == "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
