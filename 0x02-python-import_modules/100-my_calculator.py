6bb4b65eed54a07e43394760bc6c71e80097ba0e
mug3njutsu <johngitahi272@gmail.com>
2022-09-23 01:35:54 +0300

    Task 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 
-import sys
 from calculator_1 import add,sub,div,mul
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
         operator = sys.argv[2]
         if operator == "+":
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
             print("{} {} {} = {}".format(a, operator, b, add(a, b)))
         elif operator == "-":
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
             print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
         elif operator == "/":
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
             print("{} {} {} = {}".format(a, operator, b, div(a, b)))
-        elif len(operator) != 1:
-            a = int(sys.argv[1])
-            b = int(sys.argv[-1])
-            print("{} {} {} = {}".format(a, "*", b, mul(a, b)))
+        elif operator == "*":
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
