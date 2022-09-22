9cfe8327a9a34582438266cf87385876a3a710b7
mug3njutsu <johngitahi272@gmail.com>
2022-09-23 01:40:08 +0300

    Task 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     args = len(sys.argv) - 1
-    if len(sys.argv) != 3:
+    if len(args) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
