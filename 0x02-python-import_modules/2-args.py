54f793823d540ee6911f427bb88474591a93c6b8
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 14:11:08 +0300

    Task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@
 from sys import argv
 
 if __name__ == "__main__":
-    no_of_arguments = len(argv)-1
-    print(f"{no_of_arguments:d} arguments:")
+    # no_of_arguments = len(argv)-1
+    print(f"{len(argv)-1:d} arguments:")
 
     for i in range(1, len(argv)):
 	    print(f"{i:d}: {argv[i]}")
