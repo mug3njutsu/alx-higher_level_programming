c3ba9153ff76d6767d4c837e789e2a6fa86a58f1
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 13:27:09 +0300

    Task 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python3
 
 import sys
-import calculator_1
-
-def main():
-    ops = ['+', '-', '*', '/']
-    argc = len(argv)
-
-    if (argc - 1) < 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
-    elif argv[2] not in ops:
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
-    else:
-        a, b = int(argv[1]), int(argv[3])
-        if argv[2] == '+':
-            addition = add(a, b)
-            print(f'{a:d} + {b:d} = {addition:d}')
-        elif argv[2] == '-':
-            diff = sub(a, b)
-            print(f'{a:d} - {b:d} = {diff:d}')
-        elif argv[2] == '*':
-            multiplication = mul(a, b)
-            print(f'{a:d} * {b:d} = {multiplication:d}')
-        else:
-            quotient = div(a, b)
-            print(f'{a:d} / {b:d} = {quotient:d}')
+from calculator_1 import add,sub,mul,div
 
 if __name__ == "__main__":
-    main()
+	if len(sys.argv) != 3:
+		print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+		exit(1)
+	else:
+		print(sys.argv)
