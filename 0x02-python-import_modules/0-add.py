957b8f4c879367a120c6532bd6b794df7ada2eaa
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 14:38:28 +0300

    Task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-import add_0
+from add_0 import add
 
 a = 1
 b = 2
-sum = add_0.add(a, b)
+addition = add(a, b)
 
-print(f"{a:d} + {b:d} = {sum:d}")
+print(f"{a:d} + {b:d} = {addition:d}")
