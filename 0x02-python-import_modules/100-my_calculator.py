bcaeb2e7ada8abc0b5d7dd3b163c562d3101687c
mug3njutsu <johngitahi272@gmail.com>
2022-09-23 01:49:38 +0300

    Task 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""
+
 from calculator_1 import add,sub,div,mul
 import sys
 
@@ -7,44 +7,20 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        operator = sys.argv[2]
-        if operator == "+":
-            print("{} + {} = {}".format(a, operator, b, add(a, b)))
-        elif operator == "-":
-            print("{} - {} = {}".format(a, operator, b, sub(a, b)))
-        elif operator == "/":
-            print("{} / {} = {}".format(a, operator, b, div(a, b)))
-        elif operator == "*":
-            print("{} * {} = {}".format(a, operator, b, mul(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-"""
-if __name__ == "__main__":
-    import sys
 
-    nargs = len(sys.argv) - 1
-    if nargs != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    op = sys.argv[2]
-    if op != '+' and op != '-' and op != '*' and op != '/':
+    operator = sys.argv[2]
+    if operator not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    from calculator_1 import add, sub, mul, div
+        exit(1)
+    
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-
-    if op == '+':
+    
+    if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif op == '-':
+    elif operator == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif op == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    else:
+    elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("{} * {} = {}".format(a, b, mul(a, b)))
