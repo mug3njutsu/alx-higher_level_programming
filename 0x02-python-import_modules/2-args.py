1170dcf916dc0df631d68d9623df97da1960b782
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 14:24:56 +0300

    Task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+"""
 from sys import argv
 
 if __name__ == "__main__":
@@ -12,3 +13,18 @@ if __name__ == "__main__":
         print(f"{len(argv)-1} arguments:")
         for i in range(1, len(argv)):
 	        print(f"{i:d}: {argv[i]:s}")
+"""
+from sys import argv
+i = 1
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        print('0 arguments.')
+    elif len(argv) == 2:
+        print('1 argument:')
+        print('1: {}'.format(argv[1]))
+    else:
+        print('{:d} arguments:'.format(len(argv) - 1))
+        while i < len(argv):
+            print('{:d}: {:s}'.format(i, argv[i]))
+            i += 1
