aa6be7b20108f348de11535b1f50dd9fac774396
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 13:55:59 +0300

    Task 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,8 +4,12 @@ import sys
 from calculator_1 import add,sub,mul,div
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
+	if len(sys.argv) < 3:
 		print("Usage: ./100-my_calculator.py <a> <operator> <b>")
 		exit(1)
+	elif sys.argv[2] not in "+-*/":
+		print("Unknown operator. Available operators: +, -, * and /")
+		exit(1)
 	else:
-		print(sys.argv)
+		result = eval("".join(sys.argv[1:4]))
+		print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {result}")
