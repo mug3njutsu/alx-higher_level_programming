441db26d0f76f71f825972ad571bea7ba8b2289f
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 11:54:06 +0300

    Task 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+
+	no_of_arguments = len(sys.argv)-1
+	print(f"{no_of_arguments:d} arguments.")
+
+	for i in range(1, len(sys.argv)):
+		print(f"{i:d}: {sys.argv[i]}")
+
+if __name__ == "__main__":
+	main()
