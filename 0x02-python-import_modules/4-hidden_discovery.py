9571a6838392dc695a6e3d243d305ed1fa3e5042
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 12:48:17 +0300

    Task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
+import hidden_4
 
+def main():
+    for item in dir(hidden_4):
+        if item[0:2] != "__":
+            print("{:s}".format(item))
+
+if __name__ == "__main__":
+    main()
