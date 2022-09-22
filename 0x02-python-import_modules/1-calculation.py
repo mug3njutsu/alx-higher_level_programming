5cbb85ec87e8f00753d856eac3838b6385eb3610
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 14:28:34 +0300

    Task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,15 +2,16 @@
 
 from calculator_1 import add,sub,mul,div
 
-a = 10
-b = 5
+if __name__ == "__main__"
+    a = 10
+    b = 5
 
-addition = add(a, b)
-diff = sub(a, b)
-multiplication = mul(a, b)
-quotient = div(a, b)
+    addition = add(a, b)
+    diff = sub(a, b)
+    multiplication = mul(a, b)
+    quotient = div(a, b)
 
-print(f"{a:d} + {b:d} = {addition}")
-print(f"{a:d} - {b:d} = {diff}")
-print(f"{a:d} * {b:d} = {multiplication}")
-print(f"{a:d} / {b:d} = {quotient}")
+    print(f"{a:d} + {b:d} = {addition}")
+    print(f"{a:d} - {b:d} = {diff}")
+    print(f"{a:d} * {b:d} = {multiplication}")
+    print(f"{a:d} / {b:d} = {quotient}")
