0b91d41847cf75a08d99a9e84cad9db897650cfc
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 14:41:15 +0300

    Task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,6 +5,5 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    addition = add(a, b)
 
-    print(f"{a:d} + {b:d} = {addition:d}")
+    print(f"{a:d} + {b:d} = {add(a, b):d}")
