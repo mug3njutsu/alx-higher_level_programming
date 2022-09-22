511ffd577bcf73dcaa31d18057276fdbc8d3e57b
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 14:03:28 +0300

    Task 4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,11 +2,8 @@
 
 import sys
 
-def main():
-	result = ""
-	numbers = sys.argv[1:]
-	content = [int("".join(i)) for i in numbers]
-	print(sum(content))
-
 if __name__ == "__main__":
-	main()
+    result = ""
+    numbers = sys.argv[1:]
+    content = [int("".join(i)) for i in numbers]
+    print(sum(content))
