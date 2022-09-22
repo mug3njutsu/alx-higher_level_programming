f3f53941f239a86f819130a18562395fc99cbb00
mug3njutsu <johngitahi272@gmail.com>
2022-09-22 14:33:36 +0300

    Task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,9 +2,10 @@
 
 from calculator_1 import add,sub,mul,div
 
+a = 10
+b = 5
+
 if __name__ == "__main__":
-    a = 10
-    b = 5
 
     print(f"{a:d} + {b:d} = {add(a, b):d}")
     print(f"{a:d} - {b:d} = {sub(a, b):d}")
