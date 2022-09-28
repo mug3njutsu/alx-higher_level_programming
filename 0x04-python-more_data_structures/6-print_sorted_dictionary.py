e736296c6af68f08a71771f35d91456485e5e63b
mug3njutsu <johngitahi272@gmail.com>
2022-09-28 13:48:42 +0300

    Task 7

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    final = {k: a_dictionary[k] for k in sorted(a_dictionary)}
+    for key in final:
+    	print(f"{key}: {final[key]}")
