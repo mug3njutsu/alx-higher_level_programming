fb393462f79d24d2b41714844840046879d3a9f2
mug3njutsu <johngitahi272@gmail.com>
2022-09-28 23:11:59 +0300

    Task 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 
 def square_matrix_simple(matrix=[]):
     squared = []
-    for line in matrix:
-        squared.append([c**2 for c in matrix])
-        return squared
+    for row in matrix:
+        squared.append([c**2 for c in row])
+    return squared
