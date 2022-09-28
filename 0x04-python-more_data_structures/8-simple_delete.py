31a9a782babaadb612bbf334076c4cd2d7040aa1
mug3njutsu <johngitahi272@gmail.com>
2022-09-28 23:30:10 +0300

    Task 9

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        a_dictionary.pop(key)
+        del a_dictionary[key]
         return a_dictionary 
-	else:
-		return a_dictionary
+    else:
+        return a_dictionary
