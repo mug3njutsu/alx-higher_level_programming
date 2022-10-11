d071517509d937f5e3a37daae2c1ec2908361ccd
mug3njutsu <johngitahi272@gmail.com>
2022-10-11 10:36:19 +0300

    Task 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    totalNumPrinted = 0
+
+    for index in range(x):
+        try:
+            print(my_list[index], end='')
+            totalNumPrinted += 1
+        except Exception as error:
+            break
+    print('')
+    return totalNumPrinted
