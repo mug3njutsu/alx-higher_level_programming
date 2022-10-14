f85a07ddc5889e1f52b3a10455ec8d780880999e
mug3njutsu <johngitahi272@gmail.com>
2022-10-14 23:16:14 +0300

    Task 11

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import math
 """
 This module does math stuff
 It does 1+1 = 2 and that's pretty much it :)
 """
 
 
+import math
 class MagicClass:
     def __init__(self, radius):
         self.__radius = 0
