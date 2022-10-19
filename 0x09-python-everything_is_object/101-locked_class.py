85fc7c55c81eeeb62c6ec7cd5b70b881671c3d1b
mug3njutsu <johngitahi272@gmail.com>
2022-10-19 04:13:57 +0300

    Task 31

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
 class LockedClass:
-    __slots__ = ['first_name']
+    def __setattr__(self, attribute, value):
+        if attribute == "first_name":
+            self.__dict__[attribute] = value
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
