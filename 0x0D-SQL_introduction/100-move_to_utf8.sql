ea865d54c996ef4602a4db0f7824d183761948f2
mug3njutsu <johngitahi272@gmail.com>
2022-11-29 13:54:51 +0300

    Task 18

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,18 @@
 -- Converts hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci
-ALTER TABLE hbtn_0c_0.first_table CHANGE name name varchar(256) CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+-- Convert table 'first_table'
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+
+-- Convert field 'name' in 'first_table'
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CHANGE name name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
