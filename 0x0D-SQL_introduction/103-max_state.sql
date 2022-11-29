adb3b6b6a221626b634897ef6fcba6950c681d53
mug3njutsu <johngitahi272@gmail.com>
2022-11-29 14:11:35 +0300

    Task 21

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Displays max temperate
-SELECT state,max(value) AS max_temp FROM temperatures GROUP BY state;
+SELECT state,max(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
