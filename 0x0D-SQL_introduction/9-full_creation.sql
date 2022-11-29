fabcddea5b1448974a2e67d4bd7f49922aa26d28
mug3njutsu <johngitahi272@gmail.com>
2022-11-29 13:20:16 +0300

    Task 10

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ id INT,
 name varchar(256),
 score INT
 );
-INSERT INTO hbtn_0c_0.second_table (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES
 	(1, "John", 10),
 	(2, "Alex", 3),
