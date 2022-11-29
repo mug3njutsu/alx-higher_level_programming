f7c3ec2b9fca5f86f3baf12b6eb37334afc0531e
mug3njutsu <johngitahi272@gmail.com>
2022-11-29 13:58:07 +0300

    Task 18

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,7 @@
--- convert 'hbtn_0c_0' to utf8mb4 collate utf8mb4_unicode-ci
--- Convert db 'hbtn_0c-o'
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
--- Convert table 'first_table'
+-- Write a script that converts hbtn_0c_0 database to UTF8
+-- converts the database to a UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- switches the DB
 USE hbtn_0c_0;
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-
--- Convert field 'name' in 'first_table'
-USE hbtn_0c_0;
-ALTER TABLE first_table
-CHANGE name name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+-- alters the table
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
