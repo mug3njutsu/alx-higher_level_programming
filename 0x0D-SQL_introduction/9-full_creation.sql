9868f736c515729bf481d7f4b0993770f2cff8ac
mug3njutsu <johngitahi272@gmail.com>
2022-11-29 12:10:52 +0300

    Task 10

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+CREATE TABLE hbtn_0c_0.second_table IF NOT EXISTS(
+id INT,
+name varchar(256),
+score INT
+);
+INSERT INTO hbtn_0c_0.second_table (id, name, score)
+VALUES (1, "John", 10);
+VALUES (2, "Alex", 3);
+VALUES (3, "Bob", 14);
+VALUES (4, "George, 8);
