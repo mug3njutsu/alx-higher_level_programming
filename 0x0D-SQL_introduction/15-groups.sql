18efa6b20b4998107f9730dca66d9e781058a093
mug3njutsu <johngitahi272@gmail.com>
2022-11-29 12:49:55 +0300

    Task 16

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
-SELECT score, COUNT(score) as number FROM contacts GROUP BY score HAVING COUNT(score) > 1
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score HAVING COUNT(score) > 1
 ORDER BY number DESC;
