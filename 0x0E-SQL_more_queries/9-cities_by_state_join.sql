4e786a935ee7492ec0b6ee00d6e50446bb4ac259
mug3njutsu <johngitahi272@gmail.com>
2022-11-30 15:13:09 +0300

    Task 10

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities contained in htbn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON cities.state_id=states.id;
+FROM states
+INNER JOIN cities ON cities.state_id=states.id
 ORDER BY cities.id ASC;
