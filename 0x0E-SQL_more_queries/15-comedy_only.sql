f2035d5eec6ec5be1ba67e3256a25cd93e97174b
mug3njutsu <johngitahi272@gmail.com>
2022-11-30 14:30:34 +0300

    Task 16

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,5 +4,5 @@ FROM tv_shows
 INNER JOIN tv_show_genres m ON tv_shows.id = m.show_id
 INNER JOIN tv_genres g ON m.genre_id = g.id
 WHERE g.name = 'Comedy'
-ORDER BY tv_shows.title ASC; 
+ORDER BY tv_shows.title ASC;
 
