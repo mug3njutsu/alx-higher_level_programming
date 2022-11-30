6da3f90e66e6d0d46792d1d75c8903ae4be629f7
mug3njutsu <johngitahi272@gmail.com>
2022-11-30 15:05:03 +0300

    Task 7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates database htbn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+CREATE TABLE IF NOT EXISTS states(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
         name varchar(256) NOT NULL
 );
