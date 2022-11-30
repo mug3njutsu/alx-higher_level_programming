90b0d6df9c5b9dadf22ce02fbe634c0adbc3d93d
mug3njutsu <johngitahi272@gmail.com>
2022-11-30 14:19:22 +0300

    Task 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates MYSQL User user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'host';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
