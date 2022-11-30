b41e81ed8d2c228825a38ea2b668b44e833b24fa
mug3njutsu <johngitahi272@gmail.com>
2022-11-30 15:15:34 +0300

    Task 10

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- Lists all the cities contained in htbn_0d_usa
+-- Importing from the child table a column then joining it to the parent table
 SELECT cities.id, cities.name, states.name
 FROM states
 INNER JOIN cities ON cities.state_id=states.id
