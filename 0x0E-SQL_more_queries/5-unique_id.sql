9c1916505fef68e42670eb6247c682107635b577
mug3njutsu <johngitahi272@gmail.com>
2022-11-30 11:49:05 +0300

    Task 6

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,7 @@
 -- Creates table unique_id
 CREATE TABLE IF NOT EXISTS unique_id(
-	id INT NOT NULL UNIQUE,
-	name varchar(256)
+	id INT,
+	name varchar(256),
+	UNIQUE (ID)
 )
 ALTER TABLE unique_id ALTER id SET DEFAULT 1;
